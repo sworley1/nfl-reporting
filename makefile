adb282f67f801603703eff46a61ef0f48f1f6581
Sam Worley <sam.worley15@utexas.edu>
2021-10-02 15:54:04 -0500

    fixing archive workflow and correspondingly in makefile

--- makefile
+++ makefile
@@ -4,20 +4,33 @@ data :
 
 .PHONY: render
 render :
-	#./scripts/archive.sh
 	Rscript -e 'library(rmarkdown); rmarkdown::render("scripts/NFL-Report.Rmd", "rmarkdown::github_document")'
-	git config advice.addIgnoredFile false
-	git add	*
+	# mv to correct directory
+	mv scripts/NFL-Report.md ./NFL-Report.md # move .md report file to root directory
+	#mkdir NFL-Report_files/
+	mv scripts/NFL-Report_files/* NFL-Report_files/ # move support images to root directory
+
+.PHONY: git
+git :
+	git add NFL-Report.md
+	git add NFL-Report_files/*
 	git commit -m "`date +'%Y-%m-%d'`"
 	git push origin main
 
+.PHONY: archive
+archive :
+	./scripts/archive.sh
+
 clean : 
 	rm Data/play_by_play.csv
-	rm *html
+	rm -rf scripts/NFL-Report_files
+	rm scripts/*.html
 
 .PHONY: all
 all : 
 	make data
+	make archive
 	make render 
 	make clean
+	make git
 
--- scripts/archive.sh
+++ scripts/archive.sh
@@ -3,23 +3,26 @@
 # Description: responsible for archviving weekly reports into new archive folders
 ################################
 
-# move to parent directory 
-cd .. 
-
 
 # Get week and year from config files and put
 # into variables
 TMP=$(grep "NFL Landscape" NFL-Report.md)
 WEEK=$( echo $TMP | tr -dc '0-9')
 VAR="Week$WEEK"
+echo $VAR
 
-# create new file name
-mkdir archive/$VAR
-mkdir archive/$VAR/NFL-Report_files
-mkdir archive/$VAR/NFL-Report_files/figure_gfm
+# create new file directories if they don't exist
+[ ! -d archive/$VAR ] && mkdir archive/$VAR
+[ ! -d archive/$VAR/NFL-Report_files ] && mkdir archive/$VAR/NFL-Report_files
+[ ! -d archive/$VAR/NFL-Report_files/figure-gfm ] && mkdir archive/$VAR/NFL-Report_files/figure-gfm
 
 # move new files (.md & .pngs) 
 mv NFL-Report.md archive/$VAR/
+#mv NFL-Report_files/figure-gfm/* archive/$VAR/NFL-Report_files/figure-gfm/
 mv NFL-Report_files/figure-gfm/* archive/$VAR/NFL-Report_files/figure-gfm/
-mv NFL-Report_files/figure-gfm/* archive/$VAR/NFL-Report_files/figure-gfm/
+
+# push to github
+git add archive/$VAR/*
+git commit -m "adding archive for $VAR"
+git push origin main
 
