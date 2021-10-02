0229630ae1fec7877499a819618769073f6c4b95
sworley1 <sam.worley15@utexas.edu>
2021-10-02 16:22:52 -0500

    adding a dependency installation file for the R packages

---
+++ scripts/R_dependencies.R
@@ -0,0 +1,10 @@
+# File Name: R_Dependencies.R
+
+# Description: File to install R packages necessary for this application
+############################################
+
+install.packages(tidyverse)
+install.packages(nflfastR)
+install.packages(ggimage)
+install.packages(qs)
+install.packages(rmarkdown)
