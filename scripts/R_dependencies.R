0a22a1e884804dc7018e3521bda31553828f3b8c
sworley1 <sam.worley15@utexas.edu>
2021-10-02 16:57:42 -0500

    putting quotes around package installs

--- scripts/R_dependencies.R
+++ scripts/R_dependencies.R
@@ -3,8 +3,8 @@
 # Description: File to install R packages necessary for this application
 ############################################
 
-install.packages(tidyverse)
-install.packages(nflfastR)
-install.packages(ggimage)
-install.packages(qs)
-install.packages(rmarkdown)
+install.packages("tidyverse")
+install.packages("nflfastR")
+install.packages("ggimage")
+install.packages("qs")
+install.packages("rmarkdown")
